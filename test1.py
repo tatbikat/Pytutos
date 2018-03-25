e00b3e0c46fab84c005608ba9e97589b3dca9319
tatbikat <bas2nagy@hotmail.com>
2018-03-26 01:12:13 +0300

    add csv extracting data

--- test1.py
+++ test1.py
@@ -1,5 +1,17 @@
-import os
-
-print("Nothing")
-print("Nothing2")
-print("Nothing3")
+import csv
+print("Start processing....")
+barja_list = []
+with open('Voters_M6_21_Chouf.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    with open("barja.csv", "w",newline='') as outfile:
+        csvwriter = csv.writer(outfile)
+        for line in csv_reader:
+            barja_row = False
+            for k,v in line.items():
+                if k=="القرية" and v=="برجا":
+                    barja_row= True
+            if barja_row:
+                barja_list.append(line.values())
+                #csvwriter.writerow(line.values())
+        csvwriter.writerow()
+        csvwriter.writerows(barja_list)
