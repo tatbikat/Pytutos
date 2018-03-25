a851856f3f89b72da05647fbe56122767c46404c
tatbikat <bas2nagy@hotmail.com>
2018-03-26 01:54:09 +0300

    add json translations

--- test1.py
+++ test1.py
@@ -1,17 +1,23 @@
 import csv
+import json
+
+
+data = json.load(open('words.json'))
+
+print(data["village"])
 print("Start processing....")
 barja_list = []
 with open('Voters_M6_21_Chouf.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
+
     with open("barja.csv", "w",newline='') as outfile:
         csvwriter = csv.writer(outfile)
         for line in csv_reader:
             barja_row = False
             for k,v in line.items():
-                if k=="القرية" and v=="برجا":
+                if k==data["village"] and v== data["barja"]:
                     barja_row= True
             if barja_row:
                 barja_list.append(line.values())
-                #csvwriter.writerow(line.values())
-        csvwriter.writerow()
+
         csvwriter.writerows(barja_list)
