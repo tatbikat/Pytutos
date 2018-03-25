7b396fae265271995d101251b7d1b5e223a85fed
tatbikat <bas2nagy@hotmail.com>
2018-03-26 02:07:29 +0300

    add function

--- test1.py
+++ test1.py
@@ -4,20 +4,26 @@ import json
 
 data = json.load(open('words.json'))
 
-print(data["village"])
-print("Start processing....")
-barja_list = []
-with open('Voters_M6_21_Chouf.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
 
-    with open("barja.csv", "w",newline='') as outfile:
-        csvwriter = csv.writer(outfile)
-        for line in csv_reader:
-            barja_row = False
-            for k,v in line.items():
-                if k==data["village"] and v== data["barja"]:
-                    barja_row= True
-            if barja_row:
-                barja_list.append(line.values())
+def village_to_csv(village):
+    village_list = []
+    village_row = False
+    with open('Voters_M6_21_Chouf.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
 
-        csvwriter.writerows(barja_list)
+        with open(village + ".csv", "w",newline='') as outfile:
+            csvwriter = csv.writer(outfile)
+            for line in csv_reader:
+                village_row = False
+                for k,v in line.items():
+                    if k==data["village"] and v== data[village]:
+                        village_row= True
+                if village_row:
+                    village_list.append(line.values())
+
+            csvwriter.writerows(village_list)
+print("start exporting data...")
+village_to_csv("barja")
+village_to_csv("dalhoun")
+
+print("Done")
